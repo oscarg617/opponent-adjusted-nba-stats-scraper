54b70b75cd17794663f976074601570459c590cc
oscarg617 <oscargutierreza617@gmail.com>
2023-06-14 14:36:49 -0700

    team defenses stored into csv

---
+++ opponent_adjusted_nba_scraper/all_team_defense.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import concurrent.futures
+from scrapingbee import ScrapingBeeClient
+import threading
+csv_writer_lock = threading.Lock()
+
+client = ScrapingBeeClient(api_key='YIX02QIVSSSNUFEHSIFMAEA3W0SVDEXKUR9E6TLSRUS8EO54VV39E16A213PFLM9D5FIZ4CZ7GPJW4PM')
+
+BASE_URL = 'https://basketball-reference.com'
+MAX_THREADS = 10
+
+years_lst = [_ for _ in range(1980, 2023)]
+
+def scrape_defense(year):
+    url = f'/leagues/NBA_{str(year)}.html'
+    response = client.get(BASE_URL + url)
+    if response.ok:
+        soup = BeautifulSoup(response.text, features='lxml')
+        # teams_lst = soup.findAll("td", attrs={"class": "left", "data-stat": "team"}) # [td.a.get("href")[7:10] for td in soup.findAll("td", attrs={"class": "left", "data-stat": "team"})]
+        table = soup.find("table", attrs={"id": "advanced-team"})
+        teams_lst = table.find_all("td", attrs={"class": "left", "data-stat": "team"})
+        teams_lst = [a.get("href")[7:10] for a in [td for td in [td.a for td in teams_lst] if td is not None]]
+        drtg_lst =  [float(td.text) for td in table.find_all("td", attrs={"class": "right", "data-stat": "def_rtg"})]
+        table_ts_pct = soup.find("table", attrs={"id": "totals-opponent"})
+        teams_lst2 = table_ts_pct.find_all("td", attrs={"class": "left", "data-stat": "team"})
+        teams_lst2 = [a.get("href")[7:10] for a in [td for td in [td.a for td in teams_lst2] if td is not None]]
+        fga = [int(td.text) for td in table_ts_pct.find_all("td", attrs={"class": "right", "data-stat": "opp_fga"})]
+        fta = [int(td.text) for td in table_ts_pct.find_all("td", attrs={"class": "right", "data-stat": "opp_fta"})]
+        pts = [int(td.text) for td in table_ts_pct.find_all("td", attrs={"class": "right", "data-stat": "opp_pts"})]
+        ts_lst = [(pts[i] / (2 * (fga[i] + (0.44 * fta[i])))) for i in range(len(pts))]
+        drtg_tuples = [(teams_lst[i], drtg_lst[i]) for i in range(len(teams_lst))]
+        ts_tuples = [(teams_lst2[i], ts_lst[i]) for i in range(len(teams_lst2))]
+        drtg_tuples.sort(key=lambda x: x[0])
+        ts_tuples.sort(key=lambda x: x[0])
+        defense_tuples = [(ts_tuples[i][0], drtg_tuples[i][1], ts_tuples[i][1]) for i in range(len(ts_tuples))]
+        df = pd.DataFrame(defense_tuples, columns=["Team", "Def_Rtg", "TS_Pct"])
+        df['SEASON'] = year
+        df = df.iloc[:, [3, 0, 1, 2]]
+        with csv_writer_lock:
+            old_data = pd.read_csv("team_defense.csv")
+            new_data = pd.concat([old_data, df], axis=0)
+            new_data.to_csv("team_defense.csv", index=False)
+    else:
+        return response.status_code
+
+# with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+#      executor.map(scrape_defense, years_lst)
