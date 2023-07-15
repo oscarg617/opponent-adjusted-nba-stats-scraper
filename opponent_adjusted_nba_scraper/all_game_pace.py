f19bdc1103ca034006f2bfb1ce05a314390c528d
oscarg617 <oscargutierreza617@gmail.com>
2023-07-15 09:17:04 -0700

    added api key to env

--- opponent_adjusted_nba_scraper/all_game_pace.py
+++ opponent_adjusted_nba_scraper/all_game_pace.py
@@ -1,15 +1,20 @@
+import os
 import threading
 import pandas as pd
 import concurrent.futures
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 from scrapingbee import ScrapingBeeClient
-client = ScrapingBeeClient(
-    api_key='YIX02QIVSSSNUFEHSIFMAEA3W0SVDEXKUR9E6TLSRUS8EO54VV39E16A213PFLM9D5FIZ4CZ7GPJW4PM')
+
 csv_writer_lock = threading.Lock()
 
-BASE_URL = 'https://www.basketball-reference.com'
+load_dotenv()
+api_key = os.getenv("API-KEY")
+client = ScrapingBeeClient(api_key=api_key)
+
 
 MAX_THREADS = 20
+BASE_URL = "https://www.basketball-reference.com"
 
 years_lst = [_ for _ in range(1980, 2023)]
 
@@ -19,12 +24,13 @@ failed = []
 def scrape_pace(years):
     res = []
     for year in years:
-        url = f'/playoffs/NBA_{year}_games.html'
+        url = f"/playoffs/NBA_{year}_games.html"
         response = client.get(BASE_URL + url)
         if response.ok:
-            soup = BeautifulSoup(response.text, features='lxml')
+            soup = BeautifulSoup(response.text, features="lxml")
             box_scores = soup.findAll(
-                "td", attrs={"class": "center", "data-stat": "box_score_text"})
+                "td", attrs={"class": "center", "data-stat": "box_score_text"}
+            )
             res.extend([box.a.get("href") for box in box_scores])
         else:
             return response.status_code
@@ -34,20 +40,24 @@ def scrape_pace(years):
 def get_pace_dataframe(suffix):
     response = client.get(BASE_URL + suffix)
     if response.ok:
-        soup = BeautifulSoup(response.text, features='lxml')
+        soup = BeautifulSoup(response.text, features="lxml")
         teams = soup.findAll(
-            "th", attrs={"scope": "row", "class": "left", "data-stat": "team_id"})
+            "th", attrs={"scope": "row", "class": "left", "data-stat": "team_id"}
+        )
         home_team = teams[0].text
         away_team = teams[1].text
-        pace = soup.find(
-            "td", attrs={"class": "right", "data-stat": "pace"}).text
+        pace = soup.find("td", attrs={"class": "right", "data-stat": "pace"}).text
         year = suffix[11:15]
         month = suffix[15:17]
         day = suffix[17:19]
-        df = pd.DataFrame(
-            columns=["Season", "Date", "Home_Team", "Away_Team", "Pace"])
-        df.loc[len(df.index)] = [int(year), year+'-' +
-                                 month+'-'+day, home_team, away_team, pace]
+        df = pd.DataFrame(columns=["Season", "Date", "Home_Team", "Away_Team", "Pace"])
+        df.loc[len(df.index)] = [
+            int(year),
+            year + "-" + month + "-" + day,
+            home_team,
+            away_team,
+            pace,
+        ]
         with csv_writer_lock:
             old_data = pd.read_csv("game_pace.csv")
             new_data = pd.concat([old_data, df], axis=0)
