b09df20ea65668c361d43ffd1e2df7cffeb5a310
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-03-09 16:42:44 -0700

    fixed dependency versions

--- setup.py
+++ setup.py
@@ -5,20 +5,19 @@ with open("README.md", "r") as fh:
  
 setuptools.setup(
     name="opponent_adjusted_nba_scraper",
-    version="0.0.1",
+    version="1.0.3",
     author="Oscar Gutierrez Altamirano",
     author_email="oscargutierreza617@gmail.com",
-    description="A Python client for scraping data from stats.nba.com and opponent-adjusting players'gi stats.",
+    description="A Python client for scraping data from stats.nba.com and opponent-adjusting players' stats.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/oscarg617/opponent_adjusted_nba_scraper",
     packages=setuptools.find_packages(),
- 
+
     install_requires=[
-    "pandas==1.5.2",
-    "requests==2.28.1",
+    "pandas==2.1.0",
+    "requests==2.31.0",
     "six==1.16.0",
-    "Unidecode==1.3.6"
+    "Unidecode==1.3.8"
     ],
  
     license="MIT",
