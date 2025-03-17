d6a0ed034a37d5f3bcc4f48b49685033e405b9e7
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-03-17 12:08:46 -0700

    fixed assertions for list inputs

--- opponent_adjusted_nba_scraper/bball_ref/teams.py
+++ opponent_adjusted_nba_scraper/bball_ref/teams.py
@@ -1,4 +1,5 @@
 '''Finding teams within a defensive rating range from basketball reference.'''
+import sys
 import time
 import pandas as pd
 from tqdm import tqdm
@@ -9,11 +10,18 @@ except ModuleNotFoundError:
     from opponent_adjusted_nba_scraper.utils.constants import _team_to_team_abbr, SeasonType
     from opponent_adjusted_nba_scraper.bball_ref.utils import _get_dataframe
 
-def _teams_within_drtg(_drtg_range, year_range, season_type: SeasonType):
+def _teams_within_drtg(drtg_range, year_range, season_type: SeasonType):
     '''
     Returns a Pandas Dataframe of teams in a range of years within a range
     of defensive strength.
     '''
+    assert len(drtg_range) == 2
+    assert len(year_range) == 2
+    assert drtg_range[0] <= drtg_range[1]
+    assert year_range[0] <= year_range[1]
+    if year_range[0] < 1971:
+        sys.exit("Opponent data is not avaiable for teams before the 1970-71 season.")
+
     dfs = []
     for curr in tqdm(range(year_range[0], year_range[1] + 1),
                      desc="Loading opponents' stats...", ncols=75):
@@ -34,7 +42,7 @@ def _teams_within_drtg(_drtg_range, year_range, season_type: SeasonType):
         data_pd = data_pd.astype({'TEAM': 'string', 'DRTG': 'float64'})
         data_pd["OPP_TS"] = true_shooting["PTS"] / \
             (2 * (true_shooting["FGA"] + (0.44 * true_shooting["FTA"])))
-        data_pd = data_pd.query("DRTG >= @_drtg_range[0] and DRTG < @_drtg_range[1]")
+        data_pd = data_pd.query("DRTG >= @drtg_range[0] and DRTG < @drtg_range[1]")
         pd.options.mode.chained_assignment = None
         data_pd = data_pd.replace(r'\*','',regex=True).astype(str)
         data_pd = data_pd[~(data_pd["TEAM"].str.contains("League Average"))]
