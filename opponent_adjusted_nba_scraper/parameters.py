0a9f472dab230669088cf8f143f52fcbba44fa31
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-03-20 22:08:23 -0700

    added parameters to its own class

---
+++ opponent_adjusted_nba_scraper/parameters.py
@@ -0,0 +1,38 @@
+'''Dataclasses used for parameters.'''
+from dataclasses import dataclass
+
+@dataclass
+class Mode:
+    '''
+    Class for choosing stat modes.
+    '''
+
+    per_game = "PerGame"
+    per_100_poss = "Per100Poss"
+    pace_adj = "PaceAdjusted"
+    opp_adj = "OpponentAdjusted"
+    opp_pace_adj = "OpponentAndPaceAdjusted"
+
+    default = per_game
+
+@dataclass
+class SeasonType:
+    '''
+    Class for choosing season types.
+    '''
+
+    regular_season = "Regular Season"
+    playoffs = "Playoffs"
+
+    default = regular_season
+
+@dataclass
+class Site:
+    '''
+    Class for choosing site to collect data from.
+    '''
+
+    basketball_reference = "Basketball Reference"
+    nba_stats = "NBA Stats"
+
+    default = basketball_reference
