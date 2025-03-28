e07d18da3943580661fc9ac1913c3abf20de7b5a
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-03-27 18:33:30 -0700

    fixed examples after refactoring

--- examples.py
+++ examples.py
@@ -1,23 +1,20 @@
 '''Examples of usage.'''
-try:
-    from nba_stats.players import player_game_logs, player_stats
-    from nba_stats.teams import teams_within_drtg
-    from utils.constants import Mode, SeasonType
-except ModuleNotFoundError:
-    from opponent_adjusted_nba_scraper.nba_stats.players import player_game_logs, player_stats
-    from opponent_adjusted_nba_scraper.nba_stats.teams import teams_within_drtg
-    from opponent_adjusted_nba_scraper.library.constants import Mode, SeasonType
 
-df = player_game_logs('LeBron James', [2012, 2014], season_type=SeasonType.playoffs)
-print(df)
+from dans.endpoints.playerlogs import PlayerLogs
+from dans.endpoints.playerstats import PlayerStats
+from dans.endpoints.teams import Teams
+from dans.library.arguments import DataFormat, SeasonType
 
-stats_list = player_stats('Kawhi Leonard', [2019, 2021], [100, 110], \
-                          data_format=Mode.default, season_type=SeasonType.playoffs)
+logs_df = PlayerLogs('LeBron James', [2012, 2014], season_type=SeasonType.playoffs).bball_ref()
+print(logs_df)
+
+stats_list = PlayerStats('Kawhi Leonard', [2019, 2021], [100, 110], \
+                            season_type=SeasonType.playoffs).nba_stats()
 print(stats_list)
 
-stats_list = player_stats('Kevin Durant', [2017, 2019], [105, 110], \
-                          data_format=Mode.opp_pace_adj,season_type=SeasonType.playoffs)
+stats_list = PlayerStats('Kevin Durant', [2017, 2019], [105, 110], data_format=\
+                         DataFormat.opp_pace_adj, season_type=SeasonType.playoffs).nba_stats()
 print(stats_list)
 
-teams_df = teams_within_drtg([90, 102], [2001, 2004], season_type=SeasonType.default)
+teams_df = Teams([2010, 2020], [105, 110]).bball_ref()
 print(teams_df)
